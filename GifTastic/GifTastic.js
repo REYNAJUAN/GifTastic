584e65c1bd4da4f84c08e0093360a96d78a2964b
REYNAJUAN <31169278+REYNAJUAN@users.noreply.github.com>
2018-03-02 19:23:27 -0800

    uploaded files manually
    still need to wok on Ajax part of the app

---
+++ GifTastic/GifTastic.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+
+//add an array of  items to populate page//    
+    var fruits=["apples", "oranges", "lemons", "bananas", "watermelon", "grapes", "strawberries", "blueberries"];
+
+// button function//
+
+for (var i=o; i <array.lenght;i++) {
+    var fruits=$("<button>");
+   fruits.addClass(addImg);
+ fruits.attr("data=type", array[i]);
+   $(addImg).append(fruits);
+
+}
+
+$(document).on ("click.button",function(){
+    $("#fruits").empty();
+    $(".onClickButton").removeClass("active");
+    $(this).addClass("active");
+},
+
+
+ newLocal = "P5YTDFd9EVlonYHKWATRrB91bkglQ8Pt")
+//create variables to retrieve data from API//
+
+
+var APIkey=newLocal
+var queryURL="https://developers.giphy.com/dashboard/?create=true"+title+"P5YTDFd9EVlonYHKWATRrB91bkglQ8Pt";
+//THIS function?//
+console.log(imag)
+//add api url//
+
+
+$ajax({
+    url:queryURL,
+    method:"GET"
+})
+
+
+//append///
+
+});
+
+
+
+
+
+////
+
+
+
